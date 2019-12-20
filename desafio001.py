e4cded1e280e58fbe955e14c0b9910042818487a
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2019-12-20 19:46:42 -0300

    Create desafio001.py

---
+++ desafio001.py
@@ -0,0 +1,5 @@
+### Curso em Vídeo - Exercicio: desafio001.py
+### Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+nome = input("Digite o seu nome: ")
+print(f"Olá {nome} que bom te ver por aqui!")
