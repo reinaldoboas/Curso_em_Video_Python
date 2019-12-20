f88f4cfc2cd99588d23c2113599230fa8240168f
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2019-12-20 19:52:07 -0300

    Create desafio002.py

---
+++ desafio002.py
@@ -0,0 +1,10 @@
+### Curso em Vídeo - Exercicio: desafio002.py
+### Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada. 
+
+# lendo as variaveis
+dia = input("Digite o dia do mês do seu nascimento: ")
+mes = input("Digite o número do mês: ")
+ano = input("Qual ano você nasceu?")
+
+# Exibindo o resultado
+print(f"Você nasceu no dia: {dia}/{mes}/{ano}.")
