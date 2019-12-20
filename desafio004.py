e93efbb602845534377cc88515f48387989da02c
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2019-12-20 19:53:09 -0300

    Create desafio004.py

---
+++ desafio004.py
@@ -0,0 +1,12 @@
+### Curso em Vídeo - Exercicio: desafio004.py
+### Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+algo = input("Digite algo: ")
+print("O tipo primitivo desse valor é: ", type(algo))
+print("Só tem espaços?", algo.isspace())
+print("É um número?", algo.isnumeric())
+print("É alfabético?", algo.isalpha())
+print("É alfanumérico?", algo.isalnum())
+print("Está em maiúsculas?", algo.isupper())
+print("Está em minúsculas?", algo.islower())
+print("Está capitalizada?", algo.istitle())
