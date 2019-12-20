92915644cf78dc1f79eeaf8915f0dee8fa4d303d
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2019-12-20 19:52:38 -0300

    Create desafio003.py

---
+++ desafio003.py
@@ -0,0 +1,9 @@
+### Curso em Vídeo - Exercicio: desafio003.py
+### Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+print(f"A soma dos dois números {num1} e {num2} é: {soma}")
