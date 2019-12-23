061cfcda99c771c47e5aab8d159c5d08bf2f88c9
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2019-12-23 20:19:36 -0300

    Create desafio008.py

---
+++ desafio008.py
@@ -0,0 +1,9 @@
+### Curso em Vídeo - Exercicio: desafio008.py
+### Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+
+metro = int(input("Digite um valor em metros: "))
+centimetros = float(metro * 100)
+milimetros = float(metro * 1000)
+
+print(30 * "=-")
+print(f"O número digitado é {metro}\nConvertido em centimetros: {centimetros}\nConvertido em milimetros: {milimetros}")
