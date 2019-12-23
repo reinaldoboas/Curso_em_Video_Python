5706d723f94b12b9a657d1d45e12973df61763dd
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2019-12-23 20:35:59 -0300

    Create desafio009.py

---
+++ desafio009.py
@@ -0,0 +1,19 @@
+### Curso em Vídeo - Exercicio: desafio009.py
+### Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.
+
+num = int(input("Digite um número e descubra a sua tabuada: "))
+
+print(30 * "-=")
+
+print(f"{num} x 1 = {num * 1}")
+print(f"{num} x 2 = {num * 2}")
+print(f"{num} x 3 = {num * 3}")
+print(f"{num} x 4 = {num * 4}")
+print(f"{num} x 5 = {num * 5}")
+print(f"{num} x 6 = {num * 6}")
+print(f"{num} x 7 = {num * 7}")
+print(f"{num} x 8 = {num * 8}")
+print(f"{num} x 9 = {num * 9}")
+print(f"{num} x 10 = {num * 10}")
+
+print(30 * "-=")
