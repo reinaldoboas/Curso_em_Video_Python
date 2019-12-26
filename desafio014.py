27ccf08b21a8f4e2f6b85d29599afd921a05c40d
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2019-12-26 13:45:21 -0300

    Create desafio014.py

---
+++ desafio014.py
@@ -0,0 +1,9 @@
+### Curso em Vídeo - Exercicio: desafio014.py
+### Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
+
+celsius = int(input("Digite a temperatura: "))
+
+# Realizando o calculo da conversão 
+fahrenheit	= 9 * celsius / 5 + 32
+
+print(f"A temperatura {celsius} °C corresponde à {fahrenheit} °F.")
