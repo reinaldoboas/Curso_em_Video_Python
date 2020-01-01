fa68f7547ab7cfbeb37d9f6681f48dfa01dfcecf
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-01 20:55:36 -0300

    Create desafio016.py

---
+++ desafio016.py
@@ -0,0 +1,8 @@
+### Curso em Vídeo - Exercicio: desafio016.py
+### Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+### ex: Digite número: 6.127
+### O número 6.127 tem a parte inteira 6.
+
+num = float(input("Digite um número Real: "))
+
+print("O número Real digitado foi {0} e tem a parte inteira {1}".format(num, int(num)))
