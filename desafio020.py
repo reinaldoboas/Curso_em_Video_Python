4b75439476c1d1baf5711d4c1c14892d73850d8d
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-03 12:47:18 -0300

    Create desafio020.py

---
+++ desafio020.py
@@ -0,0 +1,21 @@
+### Curso em Vídeo - Exercicio: desafio020.py
+### O mesmo professor do desafio quer sortear a ordem de apresentação de trabalhos dos alunos. 
+### Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+from random import shuffle
+
+# Pegando as informações de nome dos alunos
+aluno1 = input("Digite o nome do primeiro aluno: ")
+aluno2 = input("Digite o nome do segundo aluno: ")
+aluno3 = input("Digite o nome do terceiro aluno: ")
+aluno4 = input("Digite o nome do quarto aluno: ")
+
+# Usando o módulo de embaralhamento da lista de alunos
+lista = [aluno1, aluno2, aluno3, aluno4]
+sequencia = shuffle(lista)
+
+# Exibindo os resultados
+print("=-" * 30)
+print("\nSegue a lista dos alunos com ordem sorteada:")
+print(lista)
+print("=-" * 30)
