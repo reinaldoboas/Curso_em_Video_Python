58ab64a54819ef0c73baf887707e9da5a49d6075
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-07 17:30:05 -0300

    Create desafio024.py

---
+++ desafio024.py
@@ -0,0 +1,8 @@
+### Curso em Vídeo - Exercicio: desafio024.py
+### - Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+
+# Recebendo a entrada do usuário com o nome da cidade e modificando todos os caracteres para minusculo e removendo os espaços antes e depois
+cidade = str(input("Digite o nome da cidade: ")).lower().strip()
+
+print(f"O nome da cidade digitado é: {cidade}")
+print(f"A cidade começa com o nome de Santo?", (cidade[:5] == "santo")) # realizando o teste de verdadeiro ou false comparando os 5 primeiros caracteres com a string "santo"
