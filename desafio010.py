3625506b2cda3d50dff8049ae1adcaa390f755dd
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 14:57:30 -0300

    Update desafio010.py

--- desafio010.py
+++ desafio010.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio010.py
+### Link: https://www.youtube.com/watch?v=xM4AX3Lp2mo&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=18
 ### Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar. Considere US$ 1,00 = R$ 3,27.
 
 carteira = float(input("Diga quanto você tem na carteira: R$"))
