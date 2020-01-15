57582e004af19018ad870ca0fd02abf6eae80982
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 14:56:18 -0300

    Update desafio008.py

--- desafio008.py
+++ desafio008.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio008.py
+### Link: https://www.youtube.com/watch?v=KjcdG05EAZc&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=16
 ### Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
 
 metro = int(input("Digite um valor em metros: "))
