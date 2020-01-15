7464011183de04b7140dbf202860add765bfc6c3
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 14:58:02 -0300

    Update desafio011.py

--- desafio011.py
+++ desafio011.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio011.py
+### Link: https://www.youtube.com/watch?v=mzSJpn9ldt4&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=19
 ### Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la
 ### Sabendo que cada litro de tinta, pinta uma área de 2m².
 
