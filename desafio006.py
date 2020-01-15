c3cbb37275e0dfe067cacc424bec4450cc83eee7
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 14:54:59 -0300

    Update desafio006.py

--- desafio006.py
+++ desafio006.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio006.py
+### Link: https://www.youtube.com/watch?v=mqcNw_dhl8I&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=14
 ### Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
 num = int(input("Digite um número: "))
