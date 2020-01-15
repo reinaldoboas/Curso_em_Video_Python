e8b41000c36df8a783982ffd93f9674843cdce84
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 15:00:30 -0300

    Update desafio015.py

--- desafio015.py
+++ desafio015.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio015.py
+### Link: https://www.youtube.com/watch?v=I4NYUeetLAc&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=23
 ### Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. 
 ### Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por KM rodado.
 
