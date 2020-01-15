00e7cf1635794895de58ffb23c4b2a7f89d00029
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 15:01:48 -0300

    Update desafio017.py

--- desafio017.py
+++ desafio017.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio017.py
+### Link: https://www.youtube.com/watch?v=vmPW9iWsYkY&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=26
 ### Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo,
 ### calcule e mostre o comprimento da hipotenusa.
 
