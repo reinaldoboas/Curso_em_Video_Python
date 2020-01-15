8e394f45281a8cfd7981b2b02903d982e423a831
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 15:11:46 -0300

    Update desafio003.py

--- desafio003.py
+++ desafio003.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio003.py
+### Link: https://www.youtube.com/watch?v=PB254Cfjlyk&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=3
 ### Crie um programa que leia dois números e mostre a soma entre eles.
 
 num1 = int(input("Digite o primeiro número: "))
