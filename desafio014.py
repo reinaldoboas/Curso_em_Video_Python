2021b1102101914ac5ffe04a3cb24da9c66e207d
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 14:59:50 -0300

    Update desafio014.py

--- desafio014.py
+++ desafio014.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio014.py
+### Link: https://www.youtube.com/watch?v=9l_Gay8BuAw&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=22
 ### Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
 
 celsius = int(input("Digite a temperatura: "))
