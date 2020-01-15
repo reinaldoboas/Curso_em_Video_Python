4e80906c711c48b5db1344eeb854afa0194fe9b7
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 15:01:05 -0300

    Update desafio016.py

--- desafio016.py
+++ desafio016.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio016.py
+### Link: https://www.youtube.com/watch?v=-iSbDpl5Jhw&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=25
 ### Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
 ### ex: Digite número: 6.127
 ### O número 6.127 tem a parte inteira 6.
