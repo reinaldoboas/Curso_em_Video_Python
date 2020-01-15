92e7a5876a0366eaa704279b371cff3677e466f4
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 14:59:19 -0300

    Update desafio013.py

--- desafio013.py
+++ desafio013.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio013.py
+### Link: https://www.youtube.com/watch?v=cTkivN8XcJ0&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=21
 ### Faça um algoritmo que leia o salário de um funcionario e mostre seu novo salário, com 15% de aumento.
 
 salario = float(input("Digite o seu salário atual: R$ "))
