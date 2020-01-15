a4c156951b4cfb4deda8d7c20f84db0ddd2f1ead
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 15:12:36 -0300

    Update desafio004.py

--- desafio004.py
+++ desafio004.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio004.py
+### Link: https://www.youtube.com/watch?v=tHYxjJxtJko&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=4
 ### Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
 algo = input("Digite algo: ")
