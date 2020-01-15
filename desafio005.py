5c19dde715e7c2097cf39b2efc62489946cd19fc
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 14:54:18 -0300

    Update desafio005.py

--- desafio005.py
+++ desafio005.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio005.py
+### Link: https://www.youtube.com/watch?v=664e0G_S9nU&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=13
 ### Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
 
 # lendo a entrada de dados do teclado e realizando os calculos
