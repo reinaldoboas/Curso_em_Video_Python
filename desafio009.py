8c507f78414ab450901c2c1438c008f1e5a696b0
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 14:57:00 -0300

    Update desafio009.py

--- desafio009.py
+++ desafio009.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio009.py
+### Link: https://www.youtube.com/watch?v=qajq3SI0QQs&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=17
 ### Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.
 
 num = int(input("Digite um número e descubra a sua tabuada: "))
