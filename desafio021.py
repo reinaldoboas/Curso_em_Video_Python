7182d1a97b5e2a59d4354393d3c1ed221e6e7b85
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-15 15:03:55 -0300

    Update desafio021.py

--- desafio021.py
+++ desafio021.py
@@ -1,4 +1,5 @@
 ### Curso em Vídeo - Exercicio: desafio021.py
+### Link: https://www.youtube.com/watch?v=9FiEji_fzvk&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=30
 ### Faça um programa que abra e reproduza o audio de um arquivo MP3
 
 # Importando a biblioteca Pygame
