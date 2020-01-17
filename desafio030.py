6bd25e5907bfdd998e929ba2df701f828d3332df
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-01-17 10:00:42 -0300

    Create desafio030.py

---
+++ desafio030.py
@@ -0,0 +1,12 @@
+### Curso em Vídeo - Exercicio: desafio030.py
+### Link: https://www.youtube.com/watch?v=4vFCzKuHOn4&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=31
+### Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou IMPAR.
+
+num = int(input("Digite um número: "))
+
+print("=-" * 7)
+if num % 2 == 0:
+    print("PAR!")
+else:
+    print("IMPAR!")
+print("=-" * 7)
