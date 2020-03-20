252eb0acddc7a2f43fa6d186c80d7fea9cc672bb
reinaldoboas <39996295+reinaldoboas@users.noreply.github.com>
2020-03-20 13:31:46 -0300

    Create desafio047.py

---
+++ Mundo_2/desafio047.py
@@ -0,0 +1,8 @@
+### Curso em Vídeo - Exercicio: desafio047.py
+### Link: https://www.youtube.com/watch?v=Qws8-E-YrlY
+### Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+# Fazendo um laço de repetição de 0 até 51, pulando de 2 em 2 para selecionar apenas os pares.
+for num in range (0, 51, 2):
+    print(num, end=' - ')
+print("FIM")
